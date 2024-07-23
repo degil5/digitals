fa5238628d4726e613e4ba9bd478d383c2dd616b
degil5 <144648469+degil5@users.noreply.github.com>
2024-07-23 15:03:40 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
 </template>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=imkut+antiqua:wght@300;400;500;600;700;800;900&dislay=swap");
+
 head {
   font-family: "inknut Antiqua";
 }
